52fe78c810912e434dc2a490d0aa64ebda6428ce
KarolBurgos <karburgos@umariana.edu.co>
2017-10-13 08:46:28 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/sistemas/aplicacionhola/MainActivity.java
+++ app/src/main/java/com/example/sistemas/aplicacionhola/MainActivity.java
@@ -33,8 +33,8 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, AcercaDe.class );
         startActivity(i);
        /* String v1= et1.getText().toString();
-        String s="Hola"+(v1);
-        t2.setText(s);*/
+        String s="Hola "+(v1);
+        t2.setText(s) Karol;*/
 
     }
 }
